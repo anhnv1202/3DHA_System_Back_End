7a42c207b338f7aa3b820af333da67a6e7ffa9eb
Việt Anh Nguyễn <you@example.com>
2024-01-14 01:50:57 +0700

    fix lint

--- src/modules/token/token.module.ts
+++ src/modules/token/token.module.ts
@@ -1,3 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Token, TokenSchema } from '../../models/token.model';
+import { TokenController } from './token.controller';
+import { TokensRepository } from './token.repository';
+import { TokenService } from './token.service';
+
 @Module({
   imports: [MongooseModule.forFeature([{ name: Token.name, schema: TokenSchema }])],
   controllers: [TokenController],
