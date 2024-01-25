0af5d865940c45090108e7fa1e7fcbc0aeaccaea
Mai Tran Huy <maitranhuy1709@gmail.com>
2024-01-25 12:25:18 +0700

    fix pipeline

--- src/modules/wishlist/wishlist.controller.ts
+++ src/modules/wishlist/wishlist.controller.ts
@@ -4,7 +4,7 @@ import { Profile } from '@common/decorators/user.decorator';
 import { User } from '@models/user.model';
 import { Body, Controller, Get, Put } from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
-import { WishlistDTO } from 'src/dto/wishList.dto';
+import { WishlistDTO } from 'src/dto/wishlist.dto';
 import { WishlistService } from './wishlist.service';
 
 @Controller('wishlist')
